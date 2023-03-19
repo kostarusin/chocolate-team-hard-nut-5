d45e02c15855207de6bddf7b0ace55b329cf2439
EvgeniyMoskovets <Caprizzon@gmail.com>
2023-03-19 21:29:13 +0200

    modal buy now
    зробив модалку, поки ще не робив js на відкриття та закриття, та іконку мастеркард не зробив, заміню в наступному коміті.
    також є зміни в Index.html style.css для скрипту та добавився файл js

--- src/swiper.js
+++ src/swiper.js
@@ -1,1 +1,22 @@
-new Swiper ('.card-slider')
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
